29d61bba38cc6f5241e7fa5ee8cd5d64ed82163f
Dai Nguyen <dai.t.nguyen@hotmail.com>
2021-01-14 20:57:45 -0800

    up-to-date

---
+++ src/Infrastructure/Mappers/AppConfigMapper.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using Infrastructure.Entities;
+using Shared.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Mappers
+{
+    public class AppConfigMapperProfile : Profile
+    {
+        public AppConfigMapperProfile()
+        {
+            CreateMap<AppConfig, AppConfigDto>()
+                .ReverseMap();
+        }
+        
+    }
+
+    public static class AppConfigMapper
+    {
+    }
+}
---
+++ src/Shared/DTOs/AppConfigDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Shared.DTOs
+{
+    public class AppConfigDto : BaseDto
+    {
+        public string Name { get; set; }
+        public string Key { get; set; }
+        public string Value { get; set; }
+    }
+}
