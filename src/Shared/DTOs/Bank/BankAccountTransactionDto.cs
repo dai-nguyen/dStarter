ed68006675adc0fb22c03f13a5af7a4999871db9
Dai Nguyen <dai.t.nguyen@hotmail.com>
2021-10-22 20:55:43 -0700

    up-to-date

--- src/Shared/DTOs/Bank/BankAccountTransactionDto.cs
+++ src/Shared/DTOs/Bank/BankAccountTransactionDto.cs
@@ -3,8 +3,7 @@
     public class BankAccountTransactionDto : BaseDto
     {
         public string Name { get; set; }
-        public double Amount { get; set; }
-        public bool IsRealized { get; set; }
+        public double Amount { get; set; }        
         public string UserId { get; set; }
         public string BankAccountId { get; set; }
     }
